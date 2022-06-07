correct normalisation

wind_turbine.py
@@ -133,7 +133,7 @@ class wind:
 	__speed_rate_mean = 0 			# m.s-1
 	__speed_rate_std  = 0.0 		# m.s-1
 	__heading_rate_mean = 0 		# deg
-	__heading_rate_std  = 0.0 		# deg
+	__heading_rate_std  = 0.1 		# deg
 	__time_step = 1 				# s
 	__seed = 10 		# the random seed to repeat the results
 	speed : float 					# m.s-1
@@ -250,7 +250,7 @@ class WindTurbineEnvironment(BaseEnvironment):
 		self.__simu.reset()
 		self.reward_obs_term \
 			= (self.__simu.reward, [self.__simu.state['wind_speed'], \
-				self.__simu.state['wind_rel_heading']*np.pi/180], \
+				self.__simu.state['wind_rel_heading']], \
 				self.__simu.state['is_terminal'])
 
 
@@ -262,7 +262,7 @@ class WindTurbineEnvironment(BaseEnvironment):
 			The first state observation from the environment.
 		"""
 		obs = [self.__simu.state['wind_speed'], \
-			self.__simu.state['wind_rel_heading']*np.pi/180]
+			self.__simu.state['wind_rel_heading']]
 		return obs
 
 
@@ -279,7 +279,7 @@ class WindTurbineEnvironment(BaseEnvironment):
 		self.__simu.step(action - 1)
 		self.reward_obs_term \
 			= (self.__simu.reward, [self.__simu.state['wind_speed'], \
-				self.__simu.state['wind_rel_heading']*np.pi/180], \
+				self.__simu.state['wind_rel_heading']], \
 				self.__simu.state['is_terminal'])
 
 		return self.reward_obs_term

wt_agent.py
@@ -460,7 +460,7 @@ def run_experiment(environment, agent, environment_parameters, \
                                 rl_step_result = rl_glue.rl_step()
                                 
                                 reward, state, action, _ = rl_step_result
-                                wind_heading = state[1] * 180/np.pi
+                                wind_heading = state[1]
 
                                 total_return += reward
                                 return_arr.append(reward)
@@ -508,7 +508,7 @@ environment_parameters = {}
 agent_parameters = {
     "num_tilings": [32],
     "num_tiles": [8],
-    "actor_step_size": [2**(-3)], #[2**(-2)],
+    "actor_step_size": [2**(-4)], #[2**(-2)],
     "critic_step_size": [2**(-1)], #[2**1],
     "avg_reward_step_size": [2**(-3)], #[2**(-6)],
     "num_actions": 3,